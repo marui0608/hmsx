979e51ea5630c188e2654cdbbfd6acf7a15740dd
Mary <1696815801@qq.com>
2020-04-27 18:03:03 +0800

    user模块完结

--- web/static/js/user/edit.js
+++ web/static/js/user/edit.js
@@ -8,6 +8,11 @@ var user_edit_ops = {
         console.log("edit eventBind")
         $(".user_edit_wrap .save").click(function(){
             console.log("edit click")
+            var btn_target = $(this)
+            if (btn_target.hasClass("disabled")) {
+                alert("请求正在进行，请稍后再试")
+                return
+            }
             var nickname_value = $(".user_edit_wrap input[name=nickname]").val()
             var email_value = $(".user_edit_wrap input[name=email]").val()
 
@@ -22,7 +27,7 @@ var user_edit_ops = {
                 alert("请输入符合规范的email")
                 return false;
             }
-
+            btn_target.addClass("disabled")
             $.ajax({
                 url:common_ops.buildPicUrl("/user/edit"),
                 type:"POST",
@@ -30,12 +35,14 @@ var user_edit_ops = {
                     "nickname":nickname_value,
                     "email":email_value,
                 },
-                datatype:"json",
+                dataType:"json",
                 success:function(resp){
+                    // common_ops.alert("编辑成功")
                     alert(resp.msg)
                     console.log(resp)
+                    btn_target.removeClass("disabled")
                 },
-                error:function(){
+                error:function(error){
                     console.log(error)
                 }
             })
