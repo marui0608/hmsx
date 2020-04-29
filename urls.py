8ece109fbbba062251c9305c01d4d69d42c44280
Mary <1696815801@qq.com>
2020-04-29 18:12:20 +0800

    会员评论完成

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from application import app
 from web.controllers.user.User import route_user
 from web.controllers.index import route_index
 from web.controllers.account.Account import route_account
+from web.controllers.member.Member import route_member
 # 拦截器的路由
 from web.interceptos.AuthInterceptor import *
 
@@ -10,3 +11,4 @@ from web.interceptos.AuthInterceptor import *
 app.register_blueprint(route_user, url_prefix="/user")
 app.register_blueprint(route_index,url_prefix='/')
 app.register_blueprint(route_account,url_prefix='/account')
+app.register_blueprint(route_member,url_prefix='/member')
---
+++ web/static/js/member/index.js
@@ -0,0 +1,88 @@
+;
+var member_index_ops = {
+    init:function(){
+        this.eventBind()
+    },
+    eventBind:function(){
+        $(".wrap_search .search").click(function(){
+            $(".wrap_search").submit()
+        })
+        var that = this;
+        $(".remove").click(function(){
+            id = $(this).attr("data")
+            console.log(id)
+            that.myAjax(id,"remove")
+            // $.ajax({
+            //     url:common_ops.buildUrl("/account/remove-or-recover"),
+            //     type:"POST",
+            //     data:{
+            //         "id":id,
+            //         "acts":"remove"
+            //     },
+            //     dataType:"json",
+            //     success:function(resp){
+            //         if(resp.code == 200){
+            //             window.location.href = common_ops.buildUrl("/account/index")
+            //         }
+            //         alert(resp.msg)
+            //         console.log(resp)
+            //     },
+            //     error:function(error){
+            //         console.log(error)
+            //     }
+            // })
+        })
+
+        $(".recover").click(function(){
+            id = $(this).attr("data")
+            console.log(id)
+            that.myAjax(id,"recover")
+            // $.ajax({
+            //     url:common_ops.buildUrl("/account/remove-or-recover"),
+            //     type:"POST",
+            //     data:{
+            //         "id":id,
+            //         "acts":"recover"
+            //     },
+            //     dataType:"json",
+            //     success:function(resp){
+            //         if(resp.code == 200){
+            //             window.location.href = common_ops.buildUrl("/account/index")
+            //         }
+            //         alert(resp.msg)
+            //         console.log(resp)
+            //     },
+            //     error:function(error){
+            //         console.log(error)
+            //     }
+            // })
+        })
+    },
+
+    // 封装ajax操作
+    myAjax:function(id,acts){
+        $.ajax({
+            url:common_ops.buildUrl("/member/remove-or-recover"),
+            type:"POST",
+            data:{
+                "id":id,
+                "acts":acts
+            },
+            dataType:"json",
+            success:function(resp){
+                if(resp.code == 200){
+                    window.location.href = common_ops.buildUrl("/member/index")
+                }
+                alert(resp.msg)
+                console.log(resp)
+            },
+            error:function(error){
+                console.log(error)
+            }
+        })
+    }
+}
+
+$(document).ready(function(){
+    member_index_ops.init()
+})
