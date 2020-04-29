92c14c9ccd48849befb0d32d956a9ab4a4317ee1
Mary <1696815801@qq.com>
2020-04-29 10:15:32 +0800

    分页

--- common/libs/Helper.py
+++ common/libs/Helper.py
@@ -19,3 +19,43 @@ def getCurrentDate():
 
 # def getCurrentDate(format = "%Y-%m-%d %H:%H:%S"):
     # return datetime.datetime.now().strftime(format)
+
+
+# 自定义分页操作
+def iPagenation(params):
+    import math
+    ret = {
+        "is_prev":True,
+        "is_next":True,
+        "from":0,
+        "end":0,
+        "current":0,
+        "total_pages":0,
+        "page_size":0,
+        "total":0,
+        "url":params['url']
+    }
+
+    total = int(params['total'])
+    page_size = int(params['page_size'])
+    page = int(params['page'])
+    total_pages = int(math.ceil(total/page_size))
+
+    if page <= 1:
+        ret['is_prev'] = False
+    if page >= total_pages:
+        ret['is_next'] = False
+
+    # 开始页码
+    ret['from'] = 1
+    # 结束页码
+    ret['end'] = total_pages
+    ret['current'] = page
+    ret['total_pages'] = total_pages
+    ret['total'] = total
+    ret['page_size'] = page_size
+
+    # 分页的核心是：生成器,生成页码
+    ret['range'] = range(ret['from'],ret['end']+1)
+
+    return ret
